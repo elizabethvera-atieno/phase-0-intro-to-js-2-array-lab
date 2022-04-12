a50e3028413cf4262ab6e5595f4156577cb41f09
elizabethvera-atieno <atienolizvera@gmail.com>
2022-04-12 23:26:23 +0300

    attempt array-lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(){
+    const newcat = [...cats, "Broom"]
+    return newcat
+}
+
+function prependCat(){
+    const prepCat = ['Arnold', ...cats]
+    return prepCat
+}
+
+function removeLastCat(){
+   let removeLast =  [...cats] 
+   removeLast .splice(-1)
+   return removeLast
+}
+
+function removeFirstCat(){
+    let removeFirst = [...cats]
+    removeFirst.splice(0,1)
+    return removeFirst
+}
+removeFirstCat()
